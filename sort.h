62783dcae89ecc53fe9c527145d97c2c000634e7
CNwante <myphilothea@gmail.com>
2024-02-15 19:26:51 -0800

    Include dbll data structure

--- sort.h
+++ sort.h
@@ -27,4 +27,20 @@ void bitonic_sort(int *array, size_t size);
 void quick_sort_hoare(int *array, size_t size);
 void sort_deck(deck_node_t **deck);
 
+/*===== Data Structures =====*/
+
+/**
+ * struct listint_s - Doubly linked list node
+ *
+ * @n: Integer stored in the node
+ * @prev: Pointer to the previous element of the list
+ * @next: Pointer to the next element of the list
+ */
+typedef struct listint_s
+{
+    const int n;
+    struct listint_s *prev;
+    struct listint_s *next;
+} listint_t;
+
 #endif /* _SORT_H_ */
