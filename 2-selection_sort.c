d894eebb2e6b52a73880e8860b92357ee5732269
CNwante <myphilothea@gmail.com>
2024-02-16 15:27:23 -0800

    Fix: correct wrong output

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -24,11 +24,12 @@ void selection_sort(int *array, size_t size)
 	{
 		min_idx = i;
 		for (j = i + 1; j < size; j++)
-		{
 			if (array[j] < array[min_idx])
 				min_idx = j;
-			if (min_idx != i)
-				swap(&array[min_idx], &array[i]);
+
+		if (min_idx != i)
+		{
+			swap(&array[min_idx], &array[i]);
 			print_array(array, size);
 		}
 	}
