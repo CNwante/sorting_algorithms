24e5b8e79ae83691e22b7f6cd99e51a19226a3b2
CNwante <myphilothea@gmail.com>
2024-02-16 13:17:27 -0800

    Update function prototypes

--- sort.h
+++ sort.h
@@ -42,5 +42,6 @@ void heap_sort(int *array, size_t size);
 void radix_sort(int *array, size_t size);
 void bitonic_sort(int *array, size_t size);
 void quick_sort_hoare(int *array, size_t size);
+void swap(int *a, int *b);
 
 #endif /* _SORT_H_ */
