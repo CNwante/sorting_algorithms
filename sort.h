ac47e6213f74a7c2ce57dd03b75e0fb360a45a4e
CNwante <myphilothea@gmail.com>
2024-02-16 03:18:20 -0800

    Update the position of structures

--- sort.h
+++ sort.h
@@ -11,6 +11,22 @@
 /*====== Standard Libraries ======*/
 #include <stdio.h>
 
+/*===== Data Structures =====*/
+
+/**
+ * struct listint_s - Doubly linked list node
+ *
+ * @n: Integer stored in the node
+ * @prev: Pointer to the previous element of the list
+ * @next: Pointer to the next element of the list
+ */
+typedef struct listint_s
+{
+    const int n;
+    struct listint_s *prev;
+    struct listint_s *next;
+} listint_t;
+
 /*====== Function Prototypes =====*/
 void print_array(const int *array, size_t size);
 void print_list(const listint_t *list);
@@ -27,21 +43,5 @@ void radix_sort(int *array, size_t size);
 void bitonic_sort(int *array, size_t size);
 void quick_sort_hoare(int *array, size_t size);
 void sort_deck(deck_node_t **deck);
-
-/*===== Data Structures =====*/
-
-/**
- * struct listint_s - Doubly linked list node
- *
- * @n: Integer stored in the node
- * @prev: Pointer to the previous element of the list
- * @next: Pointer to the next element of the list
- */
-typedef struct listint_s
-{
-    const int n;
-    struct listint_s *prev;
-    struct listint_s *next;
-} listint_t;
 
 #endif /* _SORT_H_ */
