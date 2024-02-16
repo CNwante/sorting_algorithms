33cd9dc3426218ccf3270d9328a047b661771d87
CNwante <myphilothea@gmail.com>
2024-02-16 03:25:13 -0800

    Fix: change wrong data type

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -5,3 +5,33 @@
  */
 
 #include "sort.h"
+
+/**
+ * swap - Swaps the values pointed to by the pointers
+ * @a: Pointer to int values
+ * @b: Pointer to int values
+ */
+void swap(int *a, int *b)
+{
+	int temp = *a;
+	*a = * b;
+	*b = temp;
+}
+
+/**
+ * bubble_sort - Sorts array integers in assending order.
+ * @array: Pointer to the array.
+ * @size: Size of the array.
+ */
+void bubble_sort(int *array, size_t size)
+{
+	size_t i, index;
+
+	for(i = 0; i < size - 1; i++)
+		for(index = 0; index < size - i - 1; index++)
+			if(array[index] > array[index + 1])
+			{
+				swap(&array[index], array[index + 1]);
+				print(array, size);
+			}
+}
