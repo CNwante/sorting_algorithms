463624f5a66df4d62e18aafed9cf7070aa62cd34
CNwante <myphilothea@gmail.com>
2024-02-20 19:24:40 -0800

    File: add deck.h

---
+++ deck.h
@@ -0,0 +1,10 @@
+#ifndef _DECK_H_
+#define _DECK_H_
+
+int _strcmp(const char *s1, const char *s2);
+char get_value(deck_node_t *card);
+void insertion_sort_deck_kind(deck_node_t **deck);
+void insertion_sort_deck_value(deck_node_t **deck);
+void sort_deck(deck_node_t **deck);
+
+#endif
